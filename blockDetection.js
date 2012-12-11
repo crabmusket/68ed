fa90ab8258129c39dc56a0a6d35b79d60ca5ba15
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-12-11 22:58:30 +1100

    Don't include empty lines in blocks.

--- blockDetection.js
+++ blockDetection.js
@@ -30,7 +30,7 @@ function updateBlocks() {
         }
         // Highlight the gutter with the appropriate block style.
         editor.getSession().$decorations[i] = "";
-        if (block > 0)
+        if (block > 0 && text.length > 0)
             editor.getSession().addGutterDecoration(i, "block" + (block % 10));
         // If this was a branch or jump, the next line begins a new block.
         if (isBranch(text)) {
