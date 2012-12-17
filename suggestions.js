05bb029dd1246cdf46506305f1f21f02b0dc0ab1
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-12-17 19:28:48 +1100

    Formed suggestions as a styled ul.

--- suggestions.js
+++ suggestions.js
@@ -6,21 +6,34 @@ function provideSuggestions(editor, e) {
     // If it looks like an opcode, start suggesting stuff!
     var matches = line.match(/^([a-zA-Z]+\:)?\s+([a-zA-Z]+)$/);
     if(matches) {
-    console.log(matches);
-        var codes = "";
+        var codes = [];
+        // Last group in the match is what they're currently typing.
         var needle = matches[matches.length - 1].toLowerCase();
+        // Find opcodes that start with the needle.
         for(op in opcodes68HC11) {
-            if(op.toLowerCase().indexOf(needle) == 0)
-                codes += op.toLowerCase() + "\n";
+            if(op.toLowerCase().indexOf(needle) == 0) {
+                var text = '<span class="suggestion">' + op.toLowerCase() + '</span>';
+                if(opcodes68HC11[op].hint != undefined) {
+                    text += '<span class="suggestion-desc">' + opcodes68HC11[op].hint + '</span>';
+                }
+                codes.push(text);
+            }
         }
+        if(codes.length < 2) {
+            setHintDialog();
+            return;
+        }
+        // Display matches in hint dialog for now.
+        var coords = editor.renderer.textToScreenCoordinates(cursor.row, cursor.column);
         setHintDialog({
-            text: codes,
+            text: '<ul><li>' + codes.join('</li><li>') + '</li></ul>',
             pos: {
-                top: 0,
-                left: 0,
+                top: coords.pageY,
+                left: coords.pageX,
             }
         });
     }
     else
+        // Hide hint dialog when editing normally.
         setHintDialog();
 }
